95a777615185d21cab976ef4a9e572d65c704ceb
catemlitten <clitten@luc.edu>
2017-09-10 12:23:54 -0500

    Made it so it searches the whole document instead of just first match and reports how many it found of the match. It currently prints the 'words' just to show it's not matching numbers

--- Regex_Practice/comp330wip.py
+++ Regex_Practice/comp330wip.py
@@ -5,18 +5,36 @@ filehandler = open(filename, 'r') #read the file
 text = filehandler.read() #have file contents inside this var
 
 """ MatchObject = re.search(pattern, input_str, flags=0) """
-searchParamJabberwocky = r"(J\w+)\s"
-jabs = re.search(searchParamJabberwocky, text)
-print(jabs.group(0))
+regwordcount = 0;
+searchParamRegWords = r"([A-Za-z]+)\s"
+regwords = re.findall(searchParamRegWords, text)
+for words in regwords:
+    print(words)
+    regwordcount += 1
 
+print(str(regwordcount) + " instance(s) of unmarked words were found in the document.")
+
+
+hashcount = 0
 searchParamHashtag = r"(\#\w+)" #escape out pound sign
-hashtag = re.search(searchParamHashtag, text)
-print(hashtag.group(0))
+hashtag = re.findall(searchParamHashtag, text)
+for hashes in hashtag:
+    hashcount += 1
 
+print(str(hashcount) + " instance(s) of hashtags were found in the document.")
+
+dollarcount = 0
 searchParamDollars = r"(\$\w+)" #escape out dollar symbol
-dollars = re.search(searchParamDollars, text)
-print(dollars.group(0))
+dollars = re.findall(searchParamDollars, text)
+for dollar in dollars:
+    dollarcount += 1
 
+print(str(dollarcount) + " instance(s) of $ were found in the document.")
+
+numcount = 0
 searchParamNum = r"(\d+)"
-nums = re.search(searchParamNum, text)
-print(nums.group(0))
+nums = re.findall(searchParamNum, text)
+for num in nums:
+    numcount += 1
+
+print(str(numcount) + " instance(s) of number tags were found in the document.")
