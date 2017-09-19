782dc537cd74f9e0232e63815768e770873ded36
Cat <clitten@luc.edu>
2017-09-19 04:04:28 +0000

    wrote a couple new methods. want to replace OpenDir with main_searches now that I have get_notes, but something is failing when I try as it has been written. currently code is functioning, but I dislike the duplicate code btween OpenDir and get_notes.

--- Regex_Practice/comp330wip.py
+++ Regex_Practice/comp330wip.py
@@ -1,6 +1,7 @@
 import re
 import os
 import locale
+import collections
 from graphoutline import *
 
 locale.getpreferredencoding()
@@ -11,21 +12,17 @@ agg_counts = {}
 #put aggregate counts into dict
 
 runnableReports = {
-     "all words" : r"([A-Za-z]+)",
      "hash tags" : r"(\#\w+)",
      "dollar signs" : r"(\$\w+)",
-     "numbers" : r"(\d+)",
      "unique identifiers" : r"(!\w+)",
      "mentions" : r"(\@\w+)",
-     "URLS" : r"([http:\/\/|https:\/\/|ftp:\/\/|w{3}].+\.\w{1,4}[\/\w]{0,300})"
+     "URLS" : r"([http:\/\/|https:\/\/|ftp:\/\/|w{3}].+\.\w{1,4}[\/\w]{0,300})",
+     "carots" : r"(\^\w+)"
 }
-#([http:\/\/|https:\/\/|ftp:\/\/|w{3}].+\.\w{1,4})
-#([http:\/\/|https:\/\/|ftp:\/\/|w{3}].+\.\w{1,4}[\/\w]{0,300})
-#((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$)
-reportChoices = ["all words", "hash tags", "dollar signs", "numbers", "unique identifiers", "mentions", "URLS", "all reports", "done running reports"]
+
+reportChoices = ["hash tags", "dollar signs", "unique identifiers", "mentions", "URLS", "carots", "which notes reference other notes", "done running reports"]
 
 #dictionary to hold the report choices (keys) and the parameters they search (values)
-#URL regex was found rather than created: @https://stackoverflow.com/questions/6038061/regular-expression-to-find-urls-within-a-string
 
 def run_keyword_type(nameOfSearch, fileText):
     search = nameOfSearch
@@ -55,12 +52,57 @@ directoryPath = os.path.dirname(os.path.realpath(__file__))
 #returns path to directory this .py file is contained in
 theOtherDirectory = directoryPath + '/TryToOpen'
 #give directory you want to open
+notes_dictionary = collections.OrderedDict()
+#keep notes in sequential order
+
+def get_notes():
+    for file in os.listdir(theOtherDirectory):
+        filename = os.fsdecode(file)
+        if filename.endswith('.txt'):
+            print("Opening and reading: " + filename)
+            filePath = os.path.join(theOtherDirectory, filename)
+            with open(filePath, 'r') as fileHand:
+                docText = fileHand.read()
+                notes_dictionary[filename] = docText
+                #key is name of note, value is the text of the document
 
 
-noteGraph = Graph()
-#create a graph for the notes
+
+#"unique identifiers" : r"(!\w+)",
+#! is a unique identifier. Functionally the title of the note. This is written on the assumption each note can have at most 1
+note_identifiers = {}
+
+def parse_notes_for_identifiers():
+    for key, val in notes_dictionary.items():
+        match = re.search(r"(?<=!)(\w+)", val)
+        #re.search only finds first instance
+        if match != None:
+            match = match.group(0)
+            #search returns a object. we only need the string value of the match
+            note_identifiers[key] = match
+            #name of note is key, !title is the value
+
+connections = {}
+#^ are references to other notes which have the unique identifier !
+def parse_notes_for_carots():
+    for key, val in notes_dictionary.items():
+        connections[key] = []
+        matches = re.findall(r"(?<=\^)(\w+)", val)
+        for match in matches:
+            connections[key].append(match)
+            #key is file name, append words in matches to array in value
+
+#bang = !
+def compare_carot_to_bang():
+    for hashtagfilename, hashtag in connections.items():
+        for uniquefilename, title in note_identifiers.items():
+            if hashtag.count(title) != 0:
+                print(hashtagfilename + " contains a reference to " + uniquefilename + " with the title !" + title)
 
 #below code allows to open 'TryToOpen' folder and opens all files ending with .txt
+
+
+
 def OpenDir(nameOfSeach):
     for file in os.listdir(theOtherDirectory):
         filename = os.fsdecode(file)
@@ -75,23 +117,50 @@ def OpenDir(nameOfSeach):
                     fileHand.close()
                     #don't hog resouces
                     continue
+            if(nameOfSeach == "which notes reference other notes"):
+                get_notes()
+                parse_notes_for_identifiers()
+                parse_notes_for_carots()
+                compare_carot_to_bang()
             else:
                 run_keyword_type(nameOfSeach, docText)
         else:
             continue
-       
+"""
+def main_searches(nameOfSeach):
+    for filename, text in notes_dictionary.items():
+        #val is text
+        if(nameOfSeach == "all reports"):
+            for typeOfSearch, searchParam in runnableReports.items():
+                run_report(searchParam, typeOfSearch, text)
+                fileHand.close()
+                #don't hog resouces
+                continue
+        if(nameOfSeach == "which notes reference other notes"):
+            parse_notes_for_identifiers()
+            parse_notes_for_carots()
+            compare_carot_to_bang()
+        else:
+            run_keyword_type(nameOfSeach, text)
+"""
+#the block above is quite broken, but Im working on it
 
 def report_options():
-    for i in range(9):
+    for i in range(8):
         print("[" + str(i) + "] " + str(reportChoices[i]))
     report_choice = input("What report would you like to run? ")
-    return int(report_choice)
+    try:
+        return int(report_choice)
+    except:
+        print("Invalid choice.")
+        report_options()
 #prints list of options and returns as int the index to the array which holds the key values which
 #can be fed to a search
 
+
 while True:
     selection = report_options()
-    if selection != 8:
+    if selection != 7:
         user_choice = reportChoices[selection]
         #feed the int returned from report_options into array reportChoices as the index. This will give back to
         #user_choice the string value from the array which matches the key in the dict runnableReports
@@ -100,9 +169,14 @@ while True:
     else:
         print("Exiting report menu. ")
         break
-    
-    
 """
+ 
+get_notes()
+parse_notes_for_identifiers()
+parse_notes_for_carots()
+compare_carot_to_bang()
+    
+
 1. Be able to report of all notes containing one or more keywords
 -- bool: contains_keywords
 2.  able to generate a report of all notes, organized by mention
